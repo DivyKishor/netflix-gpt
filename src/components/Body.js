ee17e9ca6dcb12fd16e4abc5a38cdc8cc9632834
DivyKishor <divykishor@hotmail.com>
2024-06-10 16:11:25 +0530

    login validation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './Login'
 import Browse from './Browse'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const Body = () => {
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const handleButtonClick = () =>{
+        // Validate the form data
+        const message = checkValidData(email.current.value,password.current.value);
+        setErrorMessage(message);
+        
+    }
+
   return (
     <div>
         <Header />
         <div className='absolute'>
             <img alt="background" src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_medium.jpg" />
         </div>
-        <form className='w-3/12 bg-black bg-opacity-70 rounded-lg absolute p-12 my-36 mx-auto right-0 left-0 text-white'>
-            <h1 className='font-bold font-3xl my-4 mx-2'>Sign In</h1>
-            <input type="text" placeholder="Email Address" 
+        <form onSubmit={(e)=>e.preventDefault()} className='w-3/12 bg-black bg-opacity-70 rounded-lg absolute p-12 my-36 mx-auto right-0 left-0 text-white'>
+            <h1 className='font-bold text-3xl my-4 mx-2'>Sign In</h1>
+            <input ref={email} type="text" placeholder="Email Address" 
                 className='p-4 my-4 w-full bg-gray-700 rounded-lg' />
-            <input type="password" placeholder='Password' 
+            <input ref={password} type="password" placeholder='Password' 
                 className='p-4 w-full my-4 bg-gray-700 rounded-lg' />
-            <button className='p-4 my-6 bg-red-800 w-full rounded-lg'>Sign In</button>
+            <p className='p-2 font-bold text-red-500'>{errorMessage}</p>
+            <button className='p-4 my-6 bg-red-800 w-full rounded-lg' onClick={handleButtonClick}>Sign In</button>
             <p className='py-4'>New to Netflix? Sign Up Now</p>
         </form>
     </div>
