cfab5ed1a1015c5b355ccb4d6ed63307d33a3fe5
DivyKishor <divykishor@hotmail.com>
2024-06-30 16:41:33 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,5 +13,5 @@ export const LOGO =
 
   export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-  export const OPENAI_API_KEY = ''
+  // export const OPENAI_API_KEY = ''
  
